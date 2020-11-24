3f2ccc8489cdb24d92aef7b3413348bb9522fc21
nicgiordani <nikgiordani@gmail.com>
2020-11-24 16:06:22 +0100

    frecce ok

--- js/main.js
+++ js/main.js
@@ -24,18 +24,24 @@ function avanti() {
   activeImage.removeClass("active");
 
   if (activeImage.hasClass("last") == true) {
-    $(".images img.active").addClass("active");
+    $(".images img.first").addClass("active");
   } else {
     activeImage.next().addClass("active");
   }
 
 }
-// 
-// function indietro() {
-//   var activeImage = $(".images img.active");
-//   var activeDot = $(".nav i.active");
-//
-//   activeImage.removeClass("active");
-//   activeImage.prev().addClass("active");
-//
-// }
+
+function indietro() {
+  var activeImage = $(".images img.active");
+  var activeDot = $(".nav i.active");
+
+  activeImage.removeClass("active");
+
+  if (activeImage.hasClass("first") == true) {
+    $(".images img.last").addClass("active");
+  } else {
+    activeImage.prev().addClass("active");
+  }
+
+
+}
